facafa27f93d2a8ac26b91ec629f1a926098a42a
jars-17 <math.it.net@gmail.com>
2024-12-11 17:56:30 +0700

    first commit

---
+++ quiz.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import messagebox
+import ast
+
+quiz=Tk()
+quiz.title('quiz')
+quiz.geometry('925x500+300+200')
+quiz.configure(bg='#fff')
+quiz.resizable(False,False)
+
+quiz.mainloop()
